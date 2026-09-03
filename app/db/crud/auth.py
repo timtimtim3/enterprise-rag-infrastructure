f08f8fc6ed5ae0f10069111905112f96ec4d1df6
timtimtim3 <timphilip.vg@gmail.com>
2026-09-03 12:04:10 +0200

    fix typing to User | None in user crud

--- app/db/crud/auth.py
+++ app/db/crud/auth.py
@@ -22,21 +22,21 @@ async def create_user(db: AsyncSession, username: str, email: str, password_hash
     return user
 
 
-async def get_user_by_email(db: AsyncSession, email: str) -> User:
+async def get_user_by_email(db: AsyncSession, email: str) -> User | None:
     result = await db.execute(
         select(User).where(User.email == email)
     )
     return result.scalar_one_or_none()
 
 
-async def get_user_by_username(db: AsyncSession, username: str) -> User:
+async def get_user_by_username(db: AsyncSession, username: str) -> User | None:
     result = await db.execute(
         select(User).where(User.username == username)
     )
     return result.scalar_one_or_none()
 
 
-async def get_user_by_user_id(db: AsyncSession, user_id: str) -> User:
+async def get_user_by_user_id(db: AsyncSession, user_id: str) -> User | None:
     result = await db.execute(
         select(User).where(User.user_id == user_id)
     )
